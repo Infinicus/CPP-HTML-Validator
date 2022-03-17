c9bc3e7bbf96f2b8367ad8b0c38fe006622aa377
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2022-03-17 20:47:26 +0700

    Increased Abstraction for simplification.
    Wasn't able to abstract it further because I have little mastery over pointers.

--- HTML Validator.cpp
+++ HTML Validator.cpp
@@ -45,61 +45,65 @@ void printStack(stack<string> string_stack)                                     
     }while(!string_stack.empty());
 }
 
+void HTMLIsValid(stack<string> string_stack)
+{
+    if(string_stack.empty()) cout << "\nDocument is valid!" << endl;              //Validation
+                else cout << "\nDocument is not valid!" << endl;
+}
+
 int main()
 {
-    ifstream htmlfile("index.html");                                                //input
+    ifstream htmlfile("index.html");                                            //input html file
     stack<string> tags_stack;
     string tags, line;
     char char_tags[ARR_SIZE];
 
-    if(htmlfile.is_open())                                                          //Checks if file exists
-    {
-        while(getline(htmlfile, line))                                              //Loops through the file
-        {
-            size_t pos_open = line.find('<');                                       //position of the tags
-
-            for(int tags_counter = 0; tags_counter < ARR_SIZE; tags_counter++)      //Loops through a line to check if there are multiple tags in one
-            {                                                                       //line. (Max 100 tags)
-                int n = 0;
-                pos_open = line.find('<', pos_open + tags_counter);                 //Position of the tags with + counter where + 1 is the next pos
-
-                if(pos_open != string::npos)                                        //check for open tag (<), if tag is found, execute.
-                {
-                    for(int i = pos_open; line[i-1] != '>'; i++)                    //Ignores the indentations and reads until '>'
-                    {
-                        //Function to print HTML as it is (for checking)
-                        //printHTML(line[i]);
-
-                        if(line[i] != '<' && line[i] != '>' && line[i] != '/')      //ignore these symbols
-                        {
-                            char_tags[n] = line[i];                                 //Assign char_tags as the same value as the HTML tags
-                            n++;
-                        }
-                    }
-                    tags = char_tags;                                               //Copy the char_tags array to string since the stack is <string> type
-
-                    //Condition when stack is empty or top stack is not a closing tag and tag is not a single keyword.
-                    if((tags_stack.empty() || tags_stack.top() != tags) && isNotSingleKeyword(tags))
-                        tags_stack.push(tags);                                      //Push tags to the stack as a string.
-                    else if(tags_stack.top() == tags)                               //If the top stack is the same as tags, then
-                        tags_stack.pop();                                           //pops the top stack, "marking" it closed.
-
-                    printStack(tags_stack);                                         //Prints stack to know the current condition of stack.
-
-                    for(int i = 0; i < ARR_SIZE; i++)                               //Resets the char_tags members.
-                        char_tags[i] = NULL;
-                }
-                else
-                    break;                                                          //Breaks if no further tags are found in that line
-            }
-        }
-
-        if(tags_stack.empty()) cout << "\nDocument is valid!" << endl;              //Validation
-                    else cout << "\nDocument is not valid!" << endl;
-
-    }
+    if(htmlfile.is_open()) {}                                                   //Checks if file exists
     else
-        cout << "File not found!" << endl;                                          //ptinrs file not found
+    {
+        cout << "File not found!" << endl;
+        exit(1);
+    }
+
+    while(getline(htmlfile, line))                                              //Loops through the file
+    {
+        size_t pos_open = line.find('<');                                       //position of the tags
+
+        for(int tags_counter = 0; tags_counter < ARR_SIZE; tags_counter++)      //Loops through a line to check if there are multiple tags in one
+        {                                                                       //line. (Max 100 tags)
+            int n = 0;
+            pos_open = line.find('<', pos_open + tags_counter);                 //Position of the tags with + counter (pos + 1 is the second occurence, and so on)
+
+            if(pos_open != string::npos)                                        //check for open tag (<), if tag is found, execute.
+            {
+                for(int i = pos_open; line[i-1] != '>'; i++)                    //Ignores the indentations and reads until '>'
+                {
+                    //printHTML(line[i]);                                       ////Function to print HTML as it is (for checking)
+                    if(line[i] != '<' && line[i] != '>' && line[i] != '/')      //ignore these symbols
+                    {
+                        char_tags[n] = line[i];                                 //Assign char_tags as the same value as the HTML tags
+                        n++;
+                    }
+                }
+                tags = char_tags;                                               //Copy the char_tags array to string since the stack is <string> type
+
+                //Condition when stack is empty or top stack is not a closing tag and tag is not a single keyword.
+                if((tags_stack.empty() || tags_stack.top() != tags) && isNotSingleKeyword(tags))
+                    tags_stack.push(tags);                                      //Push tags to the stack as a string.
+                else if(tags_stack.top() == tags)                               //If the top stack is the same as tags, then
+                    tags_stack.pop();                                           //pops the top stack, "marking" it closed.
+
+                printStack(tags_stack);                                         //Prints stack to know the current condition of stack.
+
+                for(int i = 0; i < ARR_SIZE; i++)                               //Resets the char_tags members.
+                    char_tags[i] = NULL;
+            }
+            else
+                break;                                                          //Breaks if no further tags are found in that line
+        }
+    }
+
+    HTMLIsValid(tags_stack);                                                    //Final validation.
 
     return 0;
 }
